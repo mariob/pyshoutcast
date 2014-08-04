6e89d2a293101e6264771318dedd80613cf6c2c8
gcmalloc <gcmalloc@gmail.com>
2014-08-04 20:46:01 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(name='pyshoutcast',
+      version='0.7.1',
+      description='Python Shoutcast API',
+      author='Mario Boikov',
+      author_email='mario.boikov@gmail.com',
+      url='https://github.com/gcmalloc/pyshoutcast',
+      package_dir = {'': 'src'},
+      py_modules=['shoutcast'],
+     )
